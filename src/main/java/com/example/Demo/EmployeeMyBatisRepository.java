6992543ab7ace3ead460ccd94cb83591f940c2cb
webdesigner001 <71431181+webdesigner001@users.noreply.github.com>
2023-02-16 15:21:53 +0530

    Add files via upload

---
+++ src/main/java/com/example/Demo/EmployeeMyBatisRepository.java
@@ -0,0 +1,29 @@
+package com.example.Demo;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface EmployeeMyBatisRepository {
+    @Select("select * from employees")
+    public List < Employee > findAll();
+
+    @Select("SELECT * FROM employees WHERE id = #{id}")
+    public Employee findById(long id);
+
+    @Delete("DELETE FROM employees WHERE id = #{id}")
+    public int deleteById(long id);
+
+    @Insert("INSERT INTO employees(id, first_name, last_name,email_address) " +
+        " VALUES (#{id}, #{firstName}, #{lastName}, #{emailId})")
+    public int insert(Employee employee);
+
+    @Update("Update employees set first_name=#{firstName}, " +
+        " last_name=#{lastName}, email_address=#{emailId} where id=#{id}")
+    public int update(Employee employee);
+}
